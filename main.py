b1731f4d57388194ab9b178f2ca7ad159263d60c
valerianbar <valerianextra@gmail.com>
2026-03-19 22:36:59 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+
+from src.model import predict
+
+data = {
+    # ===== NUMÉRIQUES (toutes obligatoires) =====
+    "age": 23,
+    "revenu_mensuel": 2000,
+    "nombre_experiences_precedentes": 3,
+    "nombre_heures_travailless": 40,
+    "annee_experience_totale": 10,
+    "annees_dans_l_entreprise": 3,
+    "annees_dans_le_poste_actuel": 2,
+    "distance_domicile_travail": 10,
+    "nb_formations_suivies": 2,
+    "satisfaction_employee_environnement": 3,
+    "note_evaluation_precedente": 3,
+    "niveau_hierarchique_poste": 2,
+    "satisfaction_employee_nature_travail": 3,
+    "satisfaction_employee_equipe": 3,
+    "satisfaction_employee_equilibre_pro_perso": 3,
+    "note_evaluation_actuelle": 3,
+    "nombre_participation_pee": 1,
+    "nombre_employee_sous_responsabilite": 0,
+    "niveau_education": 3,
+    "annees_depuis_la_derniere_promotion": 1,
+    "annes_sous_responsable_actuel": 2,
+    "ratio_stagnation": 0.33,
+    "satisfaction_globale": 1.0,
+    "stress": 5,
+
+    # ===== CATÉGORIELLES =====
+    "genre": "M",
+    "poste": "Tech Lead",
+    "departement": "consulting",
+    "statut_marital": "Marié(e)",
+    "domaine_etude": "Infra & Cloud",
+    "frequence_deplacement": "Occasionnel",
+    "heure_supplementaires": "Oui",
+    "augementation_salaire_precedente": "15 %"
+}
+
+result = predict(data)
+print("Prediction:", result["prediction"])
+print("Probability:", result["probability"])
